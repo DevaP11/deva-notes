9151b56ae4b986dc4b89b2a829df2e638bd86492
Deva Pramod <112617358+DevaP11@users.noreply.github.com>
2024-12-12 00:28:59 +0530

    Update

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,18 @@
-'use strict';
+'use strict'
 
-;
 window.addEventListener('load', () => {
-    const gttButton = document.getElementById("totop");
-    if (!gttButton) return;
-    window.onscroll = () => {
-        if (
-            document.body.scrollTop > 300 ||
+  const gttButton = document.getElementById('totop')
+  if (!gttButton) return
+  window.onscroll = () => {
+    if (
+      document.body.scrollTop > 300 ||
             document.documentElement.scrollTop > 300
-        ) {
-            gttButton.style.visibility = "visible";
-            gttButton.style.opacity = "1";
-        } else {
-            gttButton.style.visibility = "hidden";
-            gttButton.style.opacity = "0";
-        }
-    };
-});
+    ) {
+      gttButton.style.visibility = 'visible'
+      gttButton.style.opacity = '1'
+    } else {
+      gttButton.style.visibility = 'hidden'
+      gttButton.style.opacity = '0'
+    }
+  }
+})
